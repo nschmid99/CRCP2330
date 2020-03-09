a0469eda30420a492bcd72dfcca4552f9b184cf7
nschmid99 <nschmid@smu.edu>
2020-03-09 13:19:22 -0500

    trying to figure out how to make entire screen black. currently only some of the screen turns black

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,34 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+// 1=black 0=white. when keypressed, screen=1 when keypressed=false. screen=0
+
+@8192
+  D=A
+  @n  
+   M=D  // n = RAM[0]
+
+  @i
+  M=0  // i = 0
+
+  @KBD
+  D=M
+  @i
+  D=M
+  @SCREEN
+  A=A+D
+  M=0
+  D;JEQ
+
+(LOOP)
+   @i
+   D=M
+   @SCREEN
+   M=M+D 
+   M=-1
+   @END
+   0;JMP
+
+(END)
+   @LOOP  // program’s end
+   0;JMP                                                               
